a236fc0b2dbe1b49b202949a62a98d23f6dddb46
sakthi-idx <sakthi.idx@gmail.com>
2023-10-03 17:03:41 +0530

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'jetbrains-mono': ['JetBrainsMono', 'sans'],
+      },
+    },
   },
   plugins: [],
 }
