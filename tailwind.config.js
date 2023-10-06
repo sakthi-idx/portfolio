368a6fabdc7a8d35d8e8b0e90ccac35fbf204c6d
sakthi-idx <sakthi.idx@gmail.com>
2023-10-06 23:49:25 +0530

    adding font

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        'jetbrains-mono': ['JetBrainsMono', 'sans'],
+        'jetbrains-mono': ['JetBrains Mono', 'monospace'],
       },
     },
   },
