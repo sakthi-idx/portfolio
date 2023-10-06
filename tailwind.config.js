8de86493fb4419c059daac20258296d630473c82
sakthi-idx <sakthi.idx@gmail.com>
2023-10-07 00:15:44 +0530

    add in font pkg

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,12 @@ module.exports = {
   content: [],
   theme: {
     extend: {
-      
+      fontFamily: {
+        'jetbrains-mono': ['JetBrains Mono', 'monospace'],
+      },
     },
   },
+  variants: {},
   plugins: [],
 }
 
