d202933a6c852e8c64bab669be43a6caee0892b0
sakthi-idx <sakthi.idx@gmail.com>
2023-10-06 23:58:43 +0530

    removing fontin config .js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,7 @@ module.exports = {
   content: [],
   theme: {
     extend: {
-      fontFamily: {
-        'jetbrains-mono': ['JetBrains Mono', 'monospace'],
-      },
+      
     },
   },
   plugins: [],
