55b1e3cd589273747afae676f721cd5d94495c38
sakthi-idx <sakthi.idx@gmail.com>
2024-04-28 17:10:01 +0530

    color_1

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        jets: ['JetBrains+Mono'],
+        'jetbrains-mono': ['JetBrains Mono', 'monospace'],
       },
     },
   },
